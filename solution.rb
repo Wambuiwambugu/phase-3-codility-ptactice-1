073248c744d1cf07a470b390491d502e1d5d3289
Maryanne Wambugu <2wambugu.marie@gmail.com>
2023-07-15 18:37:30 +0300

    add solition1

---
+++ solution.rb
@@ -0,0 +1,26 @@
+# Challenge 1: No ifs no buts(Toy Problem)
+def num_compare(a,b)
+    comparison = (a <=> b)
+    case comparison
+    when 1
+         "#{a} is greater thab #{b}"
+    when -1
+         "#{a} is less than #{b}"
+    when 0
+         "#{a} is equal to #{b}"
+    end
+end
+
+puts num_compare(6,7)
+
+# Challenge2: Ordered Count of Characters(Toy Problem)
+def list_occurances(string)
+    list = []
+    count = 0
+    arr []
+    string.split.each do |char|
+        if spring.split.each == char
+            count += 1
+            
+    end
+end
